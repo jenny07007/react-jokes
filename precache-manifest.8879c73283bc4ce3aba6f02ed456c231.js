7fd2b315fce3f9442e5398f2b78c6c1fa9ba57cd
jenny07007 <jennyliu07@gmail.com>
2020-01-05 01:37:45 +0800

    Updates

---
+++ precache-manifest.8879c73283bc4ce3aba6f02ed456c231.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e801f8e6aa209175c53027bf0c5c9c70",
+    "url": "/react-jokes/index.html"
+  },
+  {
+    "revision": "ae214b9041fdf6f48c22",
+    "url": "/react-jokes/static/css/main.cfa446d4.chunk.css"
+  },
+  {
+    "revision": "9127312947d3740c7e9b",
+    "url": "/react-jokes/static/js/2.83acbf77.chunk.js"
+  },
+  {
+    "revision": "ae214b9041fdf6f48c22",
+    "url": "/react-jokes/static/js/main.460941b1.chunk.js"
+  },
+  {
+    "revision": "ba7cc4382c4afd855009",
+    "url": "/react-jokes/static/js/runtime~main.adc4861a.js"
+  }
+]);
